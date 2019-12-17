044fdf3ff11658e8c96a5c9f7ac559abc4d39518
Nolan Grace <nolan.grace@lightbend.com>
2019-12-17 11:41:49 -0600

    kafka run local reader working

--- learn-lightbend-cloudflow/akka-streams/src/main/scala/learnlightbend/cloudflow/akkastreams/ExampleMerge.scala
+++ learn-lightbend-cloudflow/akka-streams/src/main/scala/learnlightbend/cloudflow/akkastreams/ExampleMerge.scala
@@ -1,20 +1,22 @@
-//package learnlightbend.cloudflow.akkastreams
-//
-//import cloudflow.akkastream._
-//import cloudflow.akkastream.util.scaladsl.MergeLogic
-//import cloudflow.streamlets._
-//import cloudflow.streamlets.avro._
-//
-//class ExampleMerge extends AkkaStreamlet {
-//
-//  //\\//\\//\\ INLETS //\\//\\//\\
-//
-//  //\\//\\//\\ OUTLETS //\\//\\//\\
-//  val out = AvroOutlet[]("out", )
-//
-//  //\\//\\//\\ SHAPE //\\//\\//\\
-//  final override val shape = StreamletShape.withInlets().withOutlets(out)
-//
-//  //\\//\\//\\ LOGIC //\\//\\//\\
-//  final override def createLogic = new MergeLogic(Vector(), out)
-//}
+package learnlightbend.cloudflow.akkastreams
+
+import cloudflow.akkastream._
+import cloudflow.akkastream.util.scaladsl.MergeLogic
+import cloudflow.streamlets._
+import cloudflow.streamlets.avro._
+import learnlightbend.data.AggregatedCallStats
+
+class ExampleMerge extends AkkaStreamlet {
+
+  //\\//\\//\\ INLETS //\\//\\//\\
+  val in1 = AvroInlet[AggregatedCallStats]("in1")
+
+  //\\//\\//\\ OUTLETS //\\//\\//\\
+  val out = AvroOutlet[AggregatedCallStats]("out")
+
+  //\\//\\//\\ SHAPE //\\//\\//\\
+  final override val shape = StreamletShape.withInlets(in1).withOutlets(out)
+
+  //\\//\\//\\ LOGIC //\\//\\//\\
+  final override def createLogic = new MergeLogic(Vector(in1), out)
+}
--- learn-lightbend-cloudflow/akka-streams/src/main/scala/learnlightbend/cloudflow/akkastreams/KakfaIngress.scala
+++ learn-lightbend-cloudflow/akka-streams/src/main/scala/learnlightbend/cloudflow/akkastreams/KakfaIngress.scala
@@ -1,18 +1,23 @@
 package learnlightbend.cloudflow.akkastreams
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.kafka.{ ConsumerSettings, Subscriptions }
+import akka.kafka.scaladsl.Consumer
+import akka.stream.scaladsl.Source
 import cloudflow.akkastream._
 import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
-import cloudflow.akkastream.util.scaladsl.HttpServerLogic
 import cloudflow.streamlets._
 import cloudflow.streamlets.avro.AvroOutlet
+import learnlightbend.data._
+import net.manub.embeddedkafka.{ EmbeddedKafka, EmbeddedKafkaConfig }
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.{ ByteArrayDeserializer, StringDeserializer }
 
 class KakfaIngress extends AkkaServerStreamlet {
 
   //\\//\\//\\ INLETS //\\//\\//\\
 
   //\\//\\//\\ OUTLETS //\\//\\//\\
-  val out = AvroOutlet[]("")
+  val out = AvroOutlet[AggregatedCallStats]("bla")
 
   //\\//\\//\\ SHAPE //\\//\\//\\
   final override val shape = StreamletShape(out)
@@ -20,8 +25,38 @@ class KakfaIngress extends AkkaServerStreamlet {
   //\\//\\//\\ LOGIC //\\//\\//\\
   final override def createLogic = new RunnableGraphStreamletLogic {
 
+    val TopicName = "test-topic"
+    setupLocalKafka()
 
-    def runnableGraph =
+    val consumerSettings =
+      ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
+        .withBootstrapServers("localhost:9092")
+        .withGroupId("group1")
+        .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
+    //    def runnableGraph = Source(0 to 100).map(x ⇒ {
+    //      log.info("Current NUm: " + x)
+    //
+    //      AggregatedCallStats(0L, 0L, 0.0, 0L)
+    //    }).to(plainSink(out))
+
+    def runnableGraph = Consumer
+      .committableSource(consumerSettings, Subscriptions.topics(TopicName)).map(x ⇒ {
+        log.info("Read Message from kafka: " + x.record.value())
+
+        AggregatedCallStats(0L, 0L, 0.0, 0L)
+      }).to(plainSink(out))
+
+    private def setupLocalKafka(): Unit = {
+      if (EmbeddedKafka.isRunning) {
+        implicit val kafkaConfig = EmbeddedKafkaConfig(kafkaPort = 9092)
+        EmbeddedKafka.createCustomTopic(TopicName)
+
+        EmbeddedKafka.publishStringMessageToKafka(TopicName, "test")
+        EmbeddedKafka.publishStringMessageToKafka(TopicName, "test1")
+        EmbeddedKafka.publishStringMessageToKafka(TopicName, "test2")
+      }
+    }
+  }
 
 }
